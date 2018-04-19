7c1d43b73972ca056739114c2d387ab9eee88fde
mshuler <mshuler@vt.edu>
2018-04-19 00:31:13 -0400

    added size and description to items and added one item. changed naming

--- GoodwillOnlineDB.sql
+++ GoodwillOnlineDB.sql
@@ -17,7 +17,7 @@ CREATE TABLE User
     email VARCHAR (128) NOT NULL,
     phone_number VARCHAR (10),      
     PRIMARY KEY (id),
-    isAdmin BIT NOT NULL
+    is_admin BIT NOT NULL
 );
 
 CREATE TABLE UserPhoto
@@ -36,6 +36,7 @@ CREATE TABLE Item
     price FLOAT (7,2) NOT NULL,
     rating FLOAT (3,2) NOT NULL,
     category VARCHAR (32) NOT NULL,
+    size VARCHAR (32) NOT NULL,
     description VARCHAR (512),
     date_published DATE NOT NULL,
     FOREIGN KEY (reserved_user) REFERENCES User(id) ON DELETE CASCADE
@@ -49,15 +50,16 @@ CREATE TABLE ItemPhoto
     FOREIGN KEY (item_id) REFERENCES Item(id) ON DELETE CASCADE
 );
 
-INSERT INTO Item (title, price, rating, category, date_published) VALUES
-('Polka Dot Slim Fit Dress Shirt', 129.00, 5.00, 'T-Shirts', '2018-04-18'),
-('Diesel Brothers Shirt', 16.98, 5.00, 'T-Shirts', '2018-04-18'),
-('Aston Martin Racing Shirt', 44.99, 5.00, 'T-Shirts', '2018-04-18'),
-('PK Subban Playoff Hockey Shirt', 19.99, 5.00, 'T-Shirts', '2018-04-18'),
-('Biker Denim Jeans', 13.99, 5.00, 'Pants', '2018-04-18'),
-('Long Gym Sweatpants', 7.99, 5.00, 'Pants', '2018-04-18'),
-('Military Tactical Combat Pants', 28.00, 5.00, 'Pants', '2018-04-18'),
-('Dark Navy Blue Khaki Pants', 11.99, 5.00, 'Pants', '2018-04-18');
+INSERT INTO Item (title, price, rating, category, size, description, date_published) VALUES
+('Polka Dot Slim Fit Dress Shirt', 129.00, 5.00, 'T-Shirt', 'M', 'Material is cotton/silk, slim fit.', '2018-04-18'),
+('Diesel Brothers Shirt', 16.98, 5.00, 'T-Shirt', 'S', 'Shirt with orange and skull logo from diesel brothers', '2018-04-18'),
+('Aston Martin Racing Shirt', 44.99, 5.00, 'T-Shirt', 'L', 'Black shirt with logos', '2018-04-18'),
+('PK Subban Playoff Hockey Shirt', 19.99, 5.00, 'T-Shirt', 'L', 'Black shirt with small hockey logo in the middle', '2018-04-18'),
+('Biker Denim Jeans', 13.99, 5.00, 'Pants', 'XL', 'Short cut, mens ripped denim jeans', '2018-04-18'),
+('Long Gym Sweatpants', 7.99, 5.00, 'Pants', 'XXL', 'Sporty sweatpants with white stripe on the side', '2018-04-18'),
+('Military Tactical Combat Pants', 28.00, 5.00, 'Pants', 'S', 'Khaki colored military pants with many pockets', '2018-04-18'),
+('Dark Navy Blue Khaki Pants', 11.99, 5.00, 'Pants', 'XS', '', '2018-04-18'),
+('Hooded Color Block Corduroy Jacket', 39.99, 5.00, 'Jacket', 'XXXL', 'Multi-colored coat with green base and yellow/red accents', '2018-04-18');
 
 INSERT INTO ItemPhoto (extension, item_id) VALUES
 ('jpeg', 1),
@@ -67,4 +69,5 @@ INSERT INTO ItemPhoto (extension, item_id) VALUES
 ('jpeg', 5),
 ('jpeg', 6),
 ('jpeg', 7),
-('jpeg', 8);
+('jpeg', 8),
+('jpg', 9);
